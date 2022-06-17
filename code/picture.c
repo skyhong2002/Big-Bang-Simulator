81472dd9d3a12ebdac5c0fee4facb91cb354614d
41047036s <93424551+41047036s@users.noreply.github.com>
2022-06-17 16:35:09 +0800

    圖片

---
+++ code/picture.c
@@ -0,0 +1,19 @@
+printf("( ☉_☉)≡☞o────。。。\n")；    //bang
+printf("🛡️");    //missed
+printf("🔫");    //gatling
+printf("🧕🏿🧕🏻\n");    //indians
+printf("Σ┗(＠ロ＠;)┛\n");    //panic
+printf("");    //catbalou
+printf("");    //wellsfargo
+printf("🐎\n");    //stagecoach
+printf("🏪\n");    //generalstore
+printf("🍻\n");    //beer
+printf("🍷🍸🍹🍺🥂\n");    //saloon
+printf("🤼\n");    //duel
+
+printf("🏴‍☠️\n");    //dead
+printf("💉\n");    //補血
+printf("💣\n")；    //dinamite
+printf("🚔\n")；    //jail
+printf("🤏\n")；    //沒有距離 
+printf("🃏\n")；    //抽牌
