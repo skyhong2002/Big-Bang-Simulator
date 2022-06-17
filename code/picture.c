7b2987a5a011eb6588a386dc741e0cb4fc22ef8e
41047036s <93424551+41047036s@users.noreply.github.com>
2022-06-17 17:34:43 +0800

    圖片完成

--- code/picture.c
+++ code/picture.c
@@ -3,73 +3,68 @@ if(strncmp("BANG", c->_name,4)== 0 )
     printf("( ☉_☉)≡☞o────。。。\n");    //bang
 }
 
-if(strncmp("missed", c->_name,4)== 0 )
+if(strncmp("MANCATO", c->_name,4)== 0 )
 {
     printf("🛡️\n");    //missed
 }
 
-if(strncmp("gatling", c->_name,4)== 0 )
+if(strncmp("GATLING", c->_name,4)== 0 )
 {
     printf("🔫\n");    //gatling
 }
 
-if(strncmp("panic", c->_name,4)== 0 )
+if(strncmp("PANICOI", c->_name,4)== 0 )
 {
     printf("Σ┗(＠ロ＠;)┛\n");    //panic
 }
 
-if(strncmp("catbalou", c->_name,4)== 0 )
+/*if(strncmp("CATBALOU", c->_name,4)== 0 )
 {
     printf("\n");    //catbalou
 }
 
-if(strncmp("wellsfargo", c->_name,4)== 0 )
+if(strncmp("WELLSFARGO", c->_name,4)== 0 )
 {
     printf("\n");    //wellsfargo
 }
-
-if(strncmp("stagecoach", c->_name,4)== 0 )
+*/
+if(strncmp("MUSTANG", c->_name,4)== 0 )
 {
     printf("🐎\n");    //stagecoach
 }
 
-if(strncmp("generalstore", c->_name,4)== 0 )
+if(strncmp("EMPORIO", c->_name,4)== 0 )
 {
     printf("🏪\n");    //generalstore
 }
 
-if(strncmp("beer", c->_name,4)== 0 )
+if(strncmp("BIRRA", c->_name,4)== 0 )
 {
     printf("🍻\n");    //beer
 }
 
-if(strncmp("saloon", c->_name,4)== 0 )
+if(strncmp("SALOON", c->_name,4)== 0 )
 {
     printf("🍷🍸🍹🍺🥂\n");    //saloon
 }
-if(strncmp("duel", c->_name,4)== 0 )
+if(strncmp("DUELLO", c->_name,4)== 0 )
 {
     printf("🤼\n");    //duel
 }
-
-if(strncmp("dead", c->_name,4)== 0 )
-{
-    printf("🏴‍☠️\n");    //dead
-}
-
-if(strncmp("補血", c->_name,4)== 0 )
+/*if(strncmp("補血", c->_name,4)== 0 )
 {
     printf("💉\n");    //補血
+}*/
+
+if(strncmp("DINAMITE", c->_name,4)== 0 )
+{
+    printf("💣\n");    //dinamite
 }
 
-if(strncmp("dinamite", c->_name,4)== 0 )
+if(strncmp("PRIGIONE", c->_name,4)== 0 )
 {
-    printf("💣\n")；    //dinamite
-}
-
-if(strncmp("jail", c->_name,4)== 0 )
-{
-    printf("🚔\n")；    //jail
+    printf("🚔\n");    //jail
 }
 
 printf("🃏\n")；    //抽牌
+printf("🏴‍☠️\n");    //dead
